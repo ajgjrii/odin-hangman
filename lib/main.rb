f8b96d33556a2758a30f182b54cdc24d49b671e7
Andrew Gaytan <andrewutmail@gmail.com>
2023-07-16 17:23:37 -0500

    Added hangman display, losing coniditions, and removed ability to reuse letters

--- lib/main.rb
+++ lib/main.rb
@@ -31,6 +31,12 @@ module Visuals
     end
   end
 
+  def the_hangman(file_name, index)
+    root_directory = File.expand_path('..', __dir__)
+    root_path = File.join(root_directory, file_name)
+    file_contents = File.readlines(root_path)
+    puts file_contents[index..index+9]
+  end
 
 
 end
@@ -42,8 +48,14 @@ module GamePlay
     loop do
       print "To guess, input a letter. \n "
       letter = gets.chomp.downcase
-      break if letter.match?(/^[a-z]$/) && letter.length == 1
-      puts "Invalid entry. Try again."
+      break if letter.match?(/^[a-z]$/) && letter.length == 1 && \
+        !letters_used.include?(letter)
+      if letters_used.include?(letter) == true
+        puts "You have used that letter already. Try again."
+      else
+        puts "Invalid entry. Try again."
+      end
+
     end
     letter
   end
@@ -58,12 +70,17 @@ module GamePlay
       end
     end
 
+    @letters_used.push(letter)
+
     if !add_to_counter
       @incorrect_guesses += 1
+      @hangman_index += 10
+      puts "OH NO! Incorrect! \n "
     end
 
     if add_to_counter
       @correct_guesses += 1
+      puts "HUZZAH! Correct! \n "
     end
   end
 
@@ -72,19 +89,27 @@ module GamePlay
       puts "Congratulations! You are victorious!"
       @game_over = true
     end
+
+    if @incorrect_guesses == 7
+      puts "FAILURE! The correct word was: #{@play}"
+      @game_over = true
+    end
   end
 
 end
 class HangMan
   include Words, Visuals, GamePlay
-  attr_accessor :play, :blanks, :incorrect_guesses, :correct_guesses, :game_over
+  attr_accessor :play, :blanks, :incorrect_guesses, :correct_guesses, \
+    :game_over, :letters_used, :hangman_index
 
   def initialize
     @play = ""
     @blanks = []
+    @letters_used = []
     @incorrect_guesses = 0
     @correct_guesses = 0
     @guesses_to_win = 0
+    @hangman_index = 0
     @game_over = false
   end
 
@@ -95,6 +120,7 @@ class HangMan
   def guesses
     puts "There have been #{@correct_guesses} correct guesses so far."
     puts "There have been #{@incorrect_guesses} incorrect guesses so far."
+    puts "The letters you have used are: #{@letters_used.join("  ")}"
   end
 
 end
@@ -106,11 +132,13 @@ def play_hangman()
   game.game_word(play) # selects a word from game_dictionary at random
   game.generate_blanks(game.play) # creates an array of blanks for "game board"
   p game.blanks # prints the letters to guess
+  game.the_hangman('hangman_display.txt',game.hangman_index)
 
   until game.game_over
     guess = game.letter_entry
     game.player_turn(guess)
     p game.blanks
+    game.the_hangman('hangman_display.txt',game.hangman_index)
     game.guesses
     game.game_over_check
   end
